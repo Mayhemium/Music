5e49041cbcd0c317de69326fb63d592fd20af7b3
mayhemium <tomzdebski@gmail.com>
2019-02-13 09:39:51 +0100

    some comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let buttons = document.getElementsByClassName("buttons")[0].children;
 
+//przypisanie callbacków do przyciskow
 Array.prototype.forEach.call(buttons, button => {
     button.addEventListener("mousedown",e =>{
         playSound(e.target.textContent.trim());
     });
 });
 
+//odtwarzanie wybranego dzwieku z wybranej sciezki
 function playSound(key,which = 0){
     if(key == undefined) return;
     document.getElementById(key+which).currentTime = 0;
@@ -18,6 +20,7 @@ function playSound(key,which = 0){
     }
 }
 
+//event klawiatury
 window.addEventListener("keypress",function(e){
     switch(e.key){
         case "q":
@@ -31,6 +34,7 @@ window.addEventListener("keypress",function(e){
     }
 });
 
+//symulowane nacisniecie klawisza
 function pressKey(key){
     playSound(key);
     document.getElementById(key).classList.add("hover");
@@ -56,7 +60,7 @@ let currentTime = [0,0,0,0];
 let recordedTimes = [[],[],[],[]];
 let recordedSounds = [[],[],[],[]];
 
-//start/stop recording
+//start/stop nagrania
 function recordStop(which){
     if(isRecoding[which]){
         document.getElementsByClassName("record")[which].innerHTML = "record";
@@ -69,24 +73,28 @@ function recordStop(which){
     isRecoding[which] = !isRecoding[which];
 }
 
-//play one recording
+//odegranie konkretnego nagrania
 function playRecording(which){
     for(let i = 0; i < recordedTimes.length; i++){
         setTimeout(e=>{playSound(recordedSounds[which][i],which)},recordedTimes[which][i]);
     }
 }
 
+//wybranie ktore nagranie ma się odtworzyć
 function select(which){
     document.getElementsByClassName("select")[which].classList.toggle("selected");
     isSelected[which] = !isSelected[which];
 }
 
+//eventy do odpalania wybranych nagrań
 document.getElementById("playSelected").addEventListener("mousedown",e=>{
     for(let i = 0; i < isRecoding.length; i++){
         if(isSelected[i])
             playRecording(i);
     }
 });
+
+//odpalenie wszystkich nagrań
 document.getElementById("playAll").addEventListener("mousedown",e=>{
     for(let i = 0; i < isRecoding.length; i++){
         playRecording(i);
